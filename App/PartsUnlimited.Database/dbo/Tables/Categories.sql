e8fe60b0558d3046338207e1622348bad39e5b5a
Maxi <maximiliano.duva@outlook.com>
2022-06-13 19:24:16 -0300

    Limpio la app, voy a trabajar con una API

--- App/PartsUnlimited.Database/dbo/Tables/Categories.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Categories] (
-    [CategoryId]  INT            IDENTITY (1, 1) NOT NULL,
-    [Name]        NVARCHAR (MAX) NOT NULL,
-    [Description] NVARCHAR (MAX) NULL,
-    [ImageUrl]    NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_dbo.Categories] PRIMARY KEY CLUSTERED ([CategoryId] ASC)
-);
-
--- App/PartsUnlimited.Database/dbo/Tables/Stores.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Stores] (
-    [StoreId] INT            IDENTITY (1, 1) NOT NULL,
-    [Name]    NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_dbo.Stores] PRIMARY KEY CLUSTERED ([StoreId] ASC)
-);
-
