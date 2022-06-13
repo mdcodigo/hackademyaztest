0adfb204f90746ff89e177b25d805c91d8200706
Maxi <maximiliano.duva@outlook.com>
2022-06-13 20:59:01 -0300

    FIX modulo SQL Server.

--- Modules/SQLServer/output.tf
+++ Modules/SQLServer/output.tf
@@ -35,7 +35,9 @@ output "connection_string" {
   value       = "Server=tcp:${azurerm_sql_server.mshack.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${azurerm_sql_server.mshack.administrator_login};Password=${azurerm_sql_server.mshack.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 
+/*
 output "random_string" {
   description = "The SQL Database Name"
   value       = random_string.str.result
 }
+*/
